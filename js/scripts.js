88c22797cdbf0afa2fd7509a0805c4d4086f8805
Gustavo <gustavovncsl@outlook.com>
2020-12-19 12:07:27 -0300

    projeto

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,5 @@
                 scrollTop: position
             }, 1200);
         });
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
